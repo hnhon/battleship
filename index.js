3f5990984f2b44745816ed679bb6fdda067b4109
hhon <alexhnhon@gmail.com>
2020-08-16 06:09:32 -0700

    add player ship, rotate, drag and drop

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const grid = document.querySelector('.grid');
+const grids = document.querySelectorAll('.grid');
+const ships = document.querySelectorAll('.ship');
+const playerGrid = document.querySelector('.player-grid');
+const computerGrid = document.querySelector('.computer-grid');
 
-grid.style.backgroundColor = 'green';
+grids[0].style.backgroundColor = 'lightBlue';
+grids[1].style.backgroundColor = 'lightBlue';
 
+
+//Create smaller grids inside larger grids
 function createGrid(grid) {
     for (i = 0; i < 100; i++) {
         let div = document.createElement('div');
@@ -11,4 +17,46 @@ function createGrid(grid) {
     }
 };
 
-createGrid(grid);
+createGrid(grids[0]);
+createGrid(grids[1]);
+
+//Rotate ships
+ships[0].addEventListener('click', function(){
+    ships[0].classList.toggle('carrier-verticle')
+})
+ships[1].addEventListener('click', function(){
+    ships[1].classList.toggle('battleship-verticle')
+})
+ships[2].addEventListener('click', function(){
+    ships[2].classList.toggle('destroyer-verticle')
+})
+ships[3].addEventListener('click', function(){
+    ships[3].classList.toggle('destroyer-verticle')
+})
+ships[4].addEventListener('click', function(){
+    ships[4].classList.toggle('patrol-boat-verticle')
+})
+
+// Drag and Drop ships
+ships.forEach(ship => {
+    ship.addEventListener('dragstart', function(ev) {
+        ev.dataTransfer.setData("text", ev.target.id)
+    })
+})
+
+playerGrid.addEventListener('dragover', function(ev){
+    ev.preventDefault()
+})
+
+playerGrid.addEventListener('drop', function(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData('text');
+    ev.target.appendChild(document.getElementById(data))
+})
+
+//Computer Generate Ships
+
+
+let randomShip = {
+    direction: 
+}
