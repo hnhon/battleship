681c10da7a494f02e95d6b2d2a77887d5252cc29
hhon <alexhnhon@gmail.com>
2020-08-16 06:32:30 -0700

    computer generate random ship

--- index.js
+++ index.js
@@ -21,42 +21,61 @@ createGrid(grids[0]);
 createGrid(grids[1]);
 
 //Rotate ships
-ships[0].addEventListener('click', function(){
+ships[0].addEventListener('click', function () {
     ships[0].classList.toggle('carrier-verticle')
 })
-ships[1].addEventListener('click', function(){
+ships[1].addEventListener('click', function () {
     ships[1].classList.toggle('battleship-verticle')
 })
-ships[2].addEventListener('click', function(){
+ships[2].addEventListener('click', function () {
     ships[2].classList.toggle('destroyer-verticle')
 })
-ships[3].addEventListener('click', function(){
+ships[3].addEventListener('click', function () {
     ships[3].classList.toggle('destroyer-verticle')
 })
-ships[4].addEventListener('click', function(){
+ships[4].addEventListener('click', function () {
     ships[4].classList.toggle('patrol-boat-verticle')
 })
 
 // Drag and Drop ships
 ships.forEach(ship => {
-    ship.addEventListener('dragstart', function(ev) {
+    ship.addEventListener('dragstart', function (ev) {
         ev.dataTransfer.setData("text", ev.target.id)
     })
 })
 
-playerGrid.addEventListener('dragover', function(ev){
+playerGrid.addEventListener('dragover', function (ev) {
     ev.preventDefault()
 })
 
-playerGrid.addEventListener('drop', function(ev) {
+playerGrid.addEventListener('drop', function (ev) {
     ev.preventDefault();
     let data = ev.dataTransfer.getData('text');
     ev.target.appendChild(document.getElementById(data))
 })
 
 //Computer Generate Ships
-
-
-let randomShip = {
-    direction: 
+function generateShip(height, width, dir) {
+    let div = document.createElement('div');
+    computerGrid.appendChild(div);
+    div.style.borderRadius = '20%'
+    div.style.backgroundColor = 'pink'
+    if (dir == 0) {
+        div.style.width = width;
+        div.style.height = height
+    } else {
+        div.style.width = height;
+        div.style.height = width;
+    }
+    console.log(dir)
 }
+
+function dir () {
+    return Math.floor(Math.random(0, 1)*2) 
+}
+
+generateShip('200px', '40px', dir());
+generateShip('160px', '40px', dir());
+generateShip('120px', '40px', dir());
+generateShip('120px', '40px', dir());
+generateShip('80px', '40px', dir());
